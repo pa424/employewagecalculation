c3a71e7068811411fc6efd9d48ac359e8da0bda1
pa424 <akashkumarsrm16@gmail.com>
2022-11-04 19:32:20 +0530

    Added:emp attendance check is added

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -1,1 +1,13 @@
 echo  welcome empWageCalculation
+#!/bin/bash -x
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "empPresent";
+else
+	 echo "empAbsent";
+fi
+
